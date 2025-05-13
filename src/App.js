2fd8a52042d284766e7843a94ef9b0862d55bf60
vanu888 <kan.it.2023.f.105@gmail.com>
2025-05-13 20:42:00 +0530

    update files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-
-function APP() {
+function App() {
   return (
     <Router>
       <div>
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -30,4 +30,4 @@ function APP() {
   );
 }
 
-export default APP;
+export default App;
