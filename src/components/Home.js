d1b91c832da01aa2f327f2baadacd07d896f204c
vanu888 <kan.it.2023.f.105@gmail.com>
2025-05-13 21:47:36 +0530

    update files

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,15 @@ function Home() {
     return (
         <div className="home-container">
             <h1>Welcome to my portfolio</h1>
-            <p>Hi, I'm joe</p>
+            <p><pre>Hi 👋, I'm Vihanga Anuththara
+
+                a self-taught Python developer with a passion for web design and open-source projects. I enjoy building applications that are both functional and aesthetically pleasing.
+
+                - 🔭 I’m currently working on improving my skills in Python and web development.
+
+                - 🗿 I’m looking to collaborate on open-source projects where I can contribute and learn from others.
+
+                - 💬 Ask me about Python, web development, or anything related to programming!</pre></p>
         </div>
     );
 }
