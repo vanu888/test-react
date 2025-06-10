739cd66d9a144539baca188b7cf4b5134b658d87
vanu888 <kan.it.2023.f.105@gmail.com>
2025-06-10 15:19:48 +0530

    Test new changes2

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,7 @@ function Home() {
             <h1>Welcome to my portfolio</h1>
             <p>Hi 👋, I'm Vihanga Anuththara</p>
             <div className='info'>
-                <p>I'm an undergraduate student currently pursuing a Higher National Diploma in IT (HNDIT), with a strong interest in software development and open source collaboration.
-
-                    I develop and share Python and Bash projects on GitHub, applying version control with Git and following clean, maintainable coding practices. I focus on building practical, problem-solving tools. Recent projects include:
-
-
-
-                    I'm eager to collaborate with developers and open to internship opportunities where I can contribute, learn, and grow in a team environment.</p>
+                <p>hello world</p>
             </div>
         </div>
     );
